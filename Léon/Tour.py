ba475c5af9c2f6460382fb829533a4b14ebf3fdc
lowpolygonum <lowpolygonum@gmail.com>
2013-02-27 23:41:48 +0000

    Clean up scripts Add Door

--- Léon/Tour.py
+++ Léon/Tour.py
@@ -4,42 +4,61 @@ from utils import *
 
 scn = bpy.context.scene
 
-def createCircle(lod, radius):
-
+def createCircle(position, lod, radius):
+	# lod = Level of Detail
 	verts = []
 	edges = []
 	faces = []
-
-	# verts = [(0,0,0)]
 	for i in range(lod) :
+		# Vertices
 		verts.append((math.cos(math.pi * 2 * (i / lod)) * radius, math.sin(math.pi * 2 * (i / lod)) * radius, 0))
+		# Edges
 		if (i + 2 > lod) :
 			edges.append((i, 0))
 		else :
 			edges.append((i, i + 1))
-			
-	object = createMesh("Tour", (0,0,0), verts, edges, faces)
+	# Create object
+	object = createMesh("Tour", position, verts, edges, faces)
 	return object
+	
+# Copy object on the scene
+def copyObject(name):
+	scn.objects.active = scn.objects[name]
+	scn.objects[name].layers[0] = True
+	scn.objects[name].select = True	
+	bpy.ops.object.duplicate_move(OBJECT_OT_duplicate={"linked":False, "mode":'TRANSLATION'}, TRANSFORM_OT_translate={"value":(0,0,0)})
+	bpy.context.active_object.show_name = False
+	scn.objects[name].select = False
+	scn.objects[name].layers[0] = False
 
-def spinObject(lod, radius, z, offset):
+# Duplicate active object around a circle
+def spinObject(lod, radius, position, offset):
 	for i in range(lod) :
-		# Duplicate
+		# Refresh
 		obj = bpy.context.active_object
-		obj.location = (math.cos(math.pi * 2 * (i / lod + offset * 1/lod)) * radius, math.sin(math.pi * 2 * (i / lod + offset * 1/lod)) * radius, z)
+		# Position
+		obj.location = \
+			(position.x + math.cos(math.pi * 2 * (i / lod + offset * 1/lod)) * radius, \
+			position.y + math.sin(math.pi * 2 * (i / lod + offset * 1/lod)) * radius, \
+			position.z)
+		# Orientation
 		obj.rotation_euler[2] = math.pi * 2 * (i / lod + offset * 1/lod)
+		# Duplicate
 		bpy.ops.object.duplicate_move(OBJECT_OT_duplicate={"linked":False, "mode":'TRANSLATION'}, TRANSFORM_OT_translate={"value":(0,0,0)})
-		
+
+# Bevel edges where 'n' is the index of a vertices and 'lod' the vertices length of a circle
 def bevelCircle(n, lod):
-        # Deselect all vertex
+        # Setup context
 		bpy.ops.object.mode_set(mode='EDIT')
 		bpy.ops.mesh.select_all(action = 'DESELECT')
 		bpy.ops.object.mode_set(mode='OBJECT')
 		ob = bpy.context.active_object
 		
-		# Select circle above remparts
+		# Select circle
 		for offset in range(lod) :
 			ob.data.vertices[n * lod + offset].select = True
-		# BEVEL
+			
+		# Bevel operation
 		bpy.ops.object.mode_set(mode='EDIT')
 		bpy.ops.mesh.bevel(offset=1, segments=1, vertex_only=False)
 		
@@ -50,156 +69,139 @@ def bevelCircle(n, lod):
 class Tour :
 	object = []
 	mesh = []
-	def __init__(self):
+	def __init__(self, position):
 		lod = 16
 		radius = 20
-		height = 100
+		heightBody = 100
 		heightBase = 4
 		offsetBase = 2
 		heightRempart = 5
 		offsetRempart = 5
 		heightWall = 20
 		
-		self.object = createCircle(lod, radius)
+		# Create first circle
+		self.object = createCircle(position, lod, radius)
 		
+		# Setup context
 		scn.objects.active = self.object
 		self.object.select = True
-		
 		bpy.ops.object.editmode_toggle()
 		bpy.ops.mesh.select_mode(use_extend=False, use_expand=False, type='VERT', action='TOGGLE')
-
 		
-		# Setp 1
+		# BOTTOM #
+		
+		# Scale
 		scaleBase = 0.9
-		# scaleBase = (radius + offsetBase) / radius
-		bpy.ops.transform.resize(value=\
-			(scaleBase, scaleBase, 1))
+		bpy.ops.transform.resize(value=(scaleBase, scaleBase, 1))
+		# Extrude
 		bpy.ops.mesh.extrude_region_move(MESH_OT_extrude_region={"mirror":False}, TRANSFORM_OT_translate={"value":\
 			(0, 0, heightBase * 0.25)})
-			
-		# scaleBase = (radius + offsetBase * 0.5) / radius
-		bpy.ops.transform.resize(value=\
-			(scaleBase, scaleBase, 1))
+		# Scale
+		bpy.ops.transform.resize(value=(scaleBase, scaleBase, 1))
+		# Extrude
 		bpy.ops.mesh.extrude_region_move(MESH_OT_extrude_region={"mirror":False}, TRANSFORM_OT_translate={"value":\
 			(0, 0, heightBase * 0.75)})
 		
-		# Step 2
-		# scaleBase = (radius * scaleBase) / radius
-		bpy.ops.transform.resize(value=\
-			(scaleBase, scaleBase, 1))
+		# BODY #
 		
+		# Scale
+		bpy.ops.transform.resize(value=(scaleBase, scaleBase, 1))
+		# Subdivise body
 		subdivision = 6
 		for sub in range(subdivision) :
 			bpy.ops.mesh.extrude_region_move(MESH_OT_extrude_region={"mirror":False}, TRANSFORM_OT_translate={"value":\
-				(0, 0, height / subdivision)})
+				(0, 0, heightBody / subdivision)})
 		
-		# Remparts
+		# OFFSET TOP #
+		
+		# Extrude
 		bpy.ops.mesh.extrude_region_move(MESH_OT_extrude_region={"mirror":False}, TRANSFORM_OT_translate={"value":\
 			(0, 0, heightRempart * 0.3)})
-			
-		# scaleRempart = (radius + offsetRempart) / radius
+		# Scale
 		scaleRempart = 1.5
-		bpy.ops.transform.resize(value=\
-			(scaleRempart, scaleRempart, 1))
+		bpy.ops.transform.resize(value=(scaleRempart, scaleRempart, 1))
+		# Extrude
 		bpy.ops.mesh.extrude_region_move(MESH_OT_extrude_region={"mirror":False}, TRANSFORM_OT_translate={"value":\
 			(0, 0, heightRempart * 0.9)})
 			
-		# Ground
+		# GROUND #
+		
+		# Extrude
 		bpy.ops.mesh.extrude_region_move(MESH_OT_extrude_region={"mirror":False}, TRANSFORM_OT_translate={"value":\
 			(0, 0, 0)})
-		bpy.ops.transform.resize(value=\
-			(0.5, 0.5, 1))
-		
+		# Scale
+		bpy.ops.transform.resize(value=(0.5, 0.5, 1))
+		# Refresh object
 		bpy.ops.object.editmode_toggle()
 		bpy.ops.object.editmode_toggle()
-		sommet = self.object.data.vertices[len(self.object.data.vertices) - 1].co.z
+		# Get positionGround from last vertex
+		positionGround = self.object.data.vertices[len(self.object.data.vertices) - 1].co.xyz
+		positionGround.x = position[0]
+		positionGround.y = position[1]
 		
-		# Walls
+		# WALLS #
+		
+		# Extrude
 		bpy.ops.mesh.extrude_region_move(MESH_OT_extrude_region={"mirror":False}, TRANSFORM_OT_translate={"value":\
 			(0, 0, heightWall)})
 			
-		# Roof
+		# ROOF #
+		
+		# Extrude
 		bpy.ops.mesh.extrude_region_move(MESH_OT_extrude_region={"mirror":False}, TRANSFORM_OT_translate={"value":\
 			(0, 0, 0)})
-		bpy.ops.transform.resize(value=\
-			(0.7, 0.7, 1))
+		# Scale
+		bpy.ops.transform.resize(value=(0.7, 0.7, 1))
+		# Extrude
 		bpy.ops.mesh.extrude_region_move(MESH_OT_extrude_region={"mirror":False}, TRANSFORM_OT_translate={"value":\
 			(0, 0, 6)})
+		# Extrude
 		bpy.ops.mesh.extrude_region_move(MESH_OT_extrude_region={"mirror":False}, TRANSFORM_OT_translate={"value":\
 			(0, 0, 0)})
-		bpy.ops.transform.resize(value=\
-			(1.2, 1.2, 1))
+		# Scale
+		bpy.ops.transform.resize(value=(1.2, 1.2, 1))
+		# Extrude
 		bpy.ops.mesh.extrude_region_move(MESH_OT_extrude_region={"mirror":False}, TRANSFORM_OT_translate={"value":\
 			(0, 0, 1)})
+		# Extrude
 		bpy.ops.mesh.extrude_region_move(MESH_OT_extrude_region={"mirror":False}, TRANSFORM_OT_translate={"value":\
 			(0, 0, 5)})
 		# Colapse vertices
 		bpy.ops.mesh.merge(type='CENTER', uvs=False)
 		
-		# EDGE MODE
-		# bpy.ops.mesh.select_mode(use_extend=False, use_expand=False, type='EDGE', action='TOGGLE')
-		# prevEdge = self.object.data.edges[0]
-		# for edge in self.object.data.edges :
-			# print(dir(edge))
-			# if (self.object.data.vertices[edge.vertices[0]].co.z == self.object.data.vertices[edge.vertices[1]].co.z) :
-				# print("poin")
-				# edge.select = True
-				# print(edge.select)
-		# Update mesh with new data
-		# self.object.data.update(calc_edges=True)
-		# self.object = bpy.context.active_object
-		
+		# BEVELS Operations #
 		bevelCircle(subdivision + 6, lod)
 		bevelCircle(subdivision + 5, lod)
 		bevelCircle(subdivision + 4, lod)
 		bevelCircle(subdivision + 3, lod)
 		bevelCircle(subdivision + 2, lod)
-			
 		
-		
-		
-		# Select last vertex
-		# self.object.data.vertices[len(self.object.data.vertices) - 1].select = True
-		
-
-		# Translate all vertex with random proportion
-		# bpy.ops.object.mode_set(mode='EDIT')
-		# bpy.ops.transform.translate(value=(0.6, 0.6, 0.6), constraint_axis=(True, True, True), constraint_orientation='GLOBAL', mirror=False, proportional='ENABLED', proportional_edit_falloff='RANDOM', proportional_size=207.965, snap=False, snap_target='CLOSEST', snap_point=(0, 0, 0), snap_align=False, snap_normal=(0, 0, 0), texture_space=False, release_confirm=True)
-		
-		# bpy.ops.object.shade_smooth()
-		bpy.ops.object.mode_set(mode='OBJECT')
-
-
-		# prevVert = self.object.data.vertices[0]
-		# for vertex in self.object.data.vertices :
-			# if (vertex.co.z == prevVert.co.z) :
-				# vertex.select = True
-			# prev
-			
+		# Setup context
 		self.object.select = False
+		bpy.ops.object.mode_set(mode='OBJECT')
 		
-		# Copy original object Rempart
-		scn.objects.active = scn.objects["Rempart"]
-		scn.objects["Rempart"].layers[0] = True
-		scn.objects["Rempart"].select = True	
-		bpy.ops.object.duplicate_move(OBJECT_OT_duplicate={"linked":False, "mode":'TRANSLATION'}, TRANSFORM_OT_translate={"value":(0,0,0)})
-		scn.objects["Rempart"].select = False
-		scn.objects["Rempart"].layers[0] = False
+		# REMPART #
+		copyObject("Rempart")
+		spinObject(lod, radius, positionGround, 0)
 		
-		# REMPART
-		spinObject(lod, radius, sommet, 0)
-		
+		# Setup context
 		bpy.ops.object.select_all(action='DESELECT')
 		
-		# Copy original object Planches
-		scn.objects.active = scn.objects["Planches"]
-		scn.objects["Planches"].layers[0] = True
-		scn.objects["Planches"].select = True
-		bpy.ops.object.duplicate_move(OBJECT_OT_duplicate={"linked":False, "mode":'TRANSLATION'}, TRANSFORM_OT_translate={"value":(0,0,0)})
-		scn.objects["Planches"].select = False
-		scn.objects["Planches"].layers[0] = False
+		# PLANCHES #
+		copyObject("Planches")
+		spinObject(lod, radius - 1, positionGround, 0.5)
 		
-		# PLANCHES
-		spinObject(lod, radius - 1, sommet, 0.5)
-
-tour = Tour()
+		# DOOR #
+		copyObject("Door")
+		offset = 1
+		obj = bpy.context.active_object
+		# Position
+		obj.location = \
+			(positionGround.x + math.cos(math.pi * 2 * (3 / lod)) * (radius * 0.5 + offset), \
+			positionGround.y + math.sin(math.pi * 2 * (3 / lod)) * (radius * 0.5 + offset), \
+			positionGround.z)
+		# Orientation
+		obj.rotation_euler[2] = math.pi * 2 * (3 / lod)
+		
+# Main
+tour = Tour((5,5,0))
