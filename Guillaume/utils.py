59513c625f3c0f3e4827954c3614c7b2dcfbcd7c
noisette.guillaume <noisette.guillaume@gmail.com>
2013-03-01 14:08:02 +0000

    Nouvelle façon de faire les murs

--- Guillaume/utils.py
+++ Guillaume/utils.py
@@ -1,4 +1,6 @@
 import bpy
+import mathutils
+import math
 
 def createMesh(name, origin, verts, edges, faces):
     # Create mesh and object
@@ -14,3 +16,99 @@ def createMesh(name, origin, verts, edges, faces):
     # Update mesh with new data
     me.update(calc_edges=True)
     return ob
+
+# Defines the position of intersection into a circle and a line
+def intersectionCircleLine(initialPosition, destinationPosition, widthWall):
+    if initialPosition.x <= destinationPosition.x + 0.0000001 and initialPosition.x >= destinationPosition.x - 0.0000001:
+        x = destinationPosition.x
+        y1 = destinationPosition.y + widthWall
+        y2 = destinationPosition.y - widthWall
+        z = destinationPosition.z
+        return (mathutils.Vector((x, y1, z)), mathutils.Vector((x, y2, z)))
+    elif initialPosition.y <= destinationPosition.y + 0.0000001 and initialPosition.y >= destinationPosition.y - 0.0000001:
+        x1 = destinationPosition.x + widthWall
+        x2 = destinationPosition.x - widthWall
+        y = destinationPosition.y
+        z = destinationPosition.z
+        return (mathutils.Vector((x1, y, z)), mathutils.Vector((x2, y, z)))
+    else:
+        # y = ax + b
+        a = (initialPosition.y - destinationPosition.y) / (initialPosition.x - destinationPosition.x)
+        b = destinationPosition.y - a * destinationPosition.x
+        
+        # Ax**2 + Bx + C = 0
+        A = 1 + a ** 2
+        B = - 2 * destinationPosition.x + 2 * a * b - 2 * a * destinationPosition.y
+        C = destinationPosition.x ** 2 + destinationPosition.y ** 2 + b ** 2 - 2 * b * destinationPosition.y - widthWall ** 2
+        
+        #Determine the delta
+        delta = B ** 2 - 4 * A * C
+        
+        if delta >= 0:
+            #Get the position
+            x1 = (- B + math.sqrt(delta)) / (2 * A)
+            y1 = a * x1 + b
+            x2 = (- B - math.sqrt(delta)) / (2 * A)
+            y2 = a * x2 + b
+            z = destinationPosition.z
+            
+            return (mathutils.Vector((x1, y1, z)), mathutils.Vector((x2, y2, z)))
+    return (None, None)
+    
+# Defines the position of intersection into two circles
+def intersectionCircleCircle(pointA, radiusA, pointB, radiusB):
+    x1 = 0
+    y1 = 0
+    x2 = 0
+    y2 = 0
+    z = pointB.z
+    if pointA.y != pointB.y:
+        # Equation Ax**2 + Bx + C = 0
+        N = (radiusB ** 2 - radiusA ** 2 - pointB.x ** 2 + pointA.x ** 2 - pointB.y ** 2 + pointA.y ** 2) / (2 * (pointA.y - pointB.y))
+        H = ((pointA.x - pointB.x) / (pointA.y - pointB.y))
+        A = H ** 2 + 1
+        B = 2 * pointA.y * H - 2 * N * H - 2 * pointA.x
+        C = pointA.x ** 2 + pointA.y ** 2 + N ** 2 - radiusA ** 2 - 2 * pointA.y * N
+        
+        # Determine the delta
+        delta = sqrt(B ** 2 - 4 * A * C)
+        
+        # Get the 2 solutions
+        x1 = (- B + delta) / (2 * A)
+        y1 = N - x1 * H
+        x2 = (- B - delta) / (2 * A)
+        y2 = N - x2 * H
+    else:
+        #Equation Ay**2 + By + C = 0
+        x1 = (radiusB ** 2 - radiusA ** 2 - pointB.x ** 2 + pointA.x ** 2) / (2 * (pointA.x - pointB.x))
+        x2 = x1
+        A = 1
+        B = - 2 * pointB.y
+        C = pointB.x ** 2 + x1 ** 2 - 2 * pointB.x * x1 + pointB.y ** 2 - radiusB ** 2
+        
+        # Determine the delta
+        delta = sqrt(B ** 2 - 4 * A * C)
+        
+        # Get the 2 solutions
+        y1 = (- B + delta) / (2 * A)
+        y2 = (- B - delta) / (2 * A)
+    
+    #Get the position
+    return (mathutils.Vector((x1, y1, z)), mathutils.Vector((x2, y2, z)))
+
+# Do the rotation around a point "origin" of point "position" an angle "angle"
+def getPositionRotationY(origin, position, angle):
+    x = math.cos(angle) * position.x - math.sin(angle) * position.y + origin.x - math.cos(angle) * origin.x + math.sin(angle) * origin.y
+    y = math.sin(angle) * position.x + math.cos(angle) * position.y + origin.y - math.sin(angle) * origin.x - math.cos(angle) * origin.y
+    z = position.z
+    
+    return mathutils.Vector((x, y, z))
+
+# Defines the visibility of a point
+def visible(self, vertex, previousVertex, previousPreviousVertex):
+    segmentActuel = mathutils.Vector((previousPreviousVertex.x - previousVertex.x, previousPreviousVertex.y - previousVertex.y, 0))
+    segmentPrecedent = mathutils.Vector((vertex.x - previousVertex.x, vertex.y - previousVertex.y, 0))
+    
+    # Orientation of the polygon, it's everytime positive
+    signOrientation = 1
+    return signOrientation * (segmentActuel.x * segmentPrecedent.y - segmentActuel.y * segmentPrecedent.x)
--- Guillaume/wallGenerator.py
+++ Guillaume/wallGenerator.py
@@ -1,218 +1,41 @@
 import bpy
-import random
 import mathutils
-from math import*
+import math
 from utils import *
 
 # Class WallGenerator
 class WallGenerator:
-    def __init__(self, positionCenter=mathutils.Vector((0, 0, 0)), distance=20, minimalWallLenght=20, maximalWallLenght=26, widthWall=4, minimalHeightWall=10, maximalHeightWall=15):
-        self.positionCenter = positionCenter
-        self.distance = distance
-        self.minimalWallLenght = minimalWallLenght
-        self.maximalWallLenght = maximalWallLenght
-        self.widthWall = widthWall
-        self.minimalHeightWall = minimalHeightWall
-        self.maximalHeightWall = maximalHeightWall
-        self.listEdges = []
-        self.listFaces = []
-        self.positionsExternalWall = []
-        self.positionsInternalWall = []
-        tempVector = mathutils.Vector((self.positionCenter.x + self.distance, self.positionCenter.y, 0))
-        self.createInternalWall(tempVector, random.uniform(0, self.distance * 2))
-        self.createExternalWall()
-        object = createMesh("WallGenerator", (0,0,0), self.positionsInternalWall + self.positionsExternalWall, self.listEdges, self.listFaces)
-        self.createWallHeight(object)
+    def __init__(self, object1, object2, wallWidth=3, wallHeight=5):
+        self.object1 = object1
+        self.object2 = object2
+        self.wallWidth = wallWidth
+        self.wallHeight = wallHeight
+        self.createWall()
     
-    # Defines the position of the point internal of the wall
-    def createInternalWall(self, initialPosition, lengthWall):
-        x1 = 0
-        y1 = 0
-        x2 = 0
-        y2 = 0
-        if self.positionCenter.y != initialPosition.y:
-            # Equation Ax**2 + Bx + C = 0
-            N = (lengthWall ** 2 - self.distance ** 2 - initialPosition.x ** 2 + self.positionCenter.x ** 2 - initialPosition.y ** 2 + self.positionCenter.y ** 2) / (2 * (self.positionCenter.y - initialPosition.y))
-            H = ((self.positionCenter.x - initialPosition.x) / (self.positionCenter.y - initialPosition.y))
-            A = H ** 2 + 1
-            B = 2 * self.positionCenter.y * H - 2 * N * H - 2 * self.positionCenter.x
-            C = self.positionCenter.x ** 2 + self.positionCenter.y ** 2 + N ** 2 - self.distance ** 2 - 2 * self.positionCenter.y * N
-            
-            # Determine the delta
-            delta = sqrt(B ** 2 - 4 * A * C)
-            
-            # Get the 2 solutions
-            x1 = (- B + delta) / (2 * A)
-            y1 = N - x1 * H
-            x2 = (- B - delta) / (2 * A)
-            y2 = N - x2 * H
-        else:
-            #Equation Ay**2 + By + C = 0
-            x1 = (lengthWall ** 2 - self.distance ** 2 - initialPosition.x ** 2 + self.positionCenter.x ** 2) / (2 * (self.positionCenter.x - initialPosition.x))
-            x2 = x1
-            A = 1
-            B = - 2 * initialPosition.y
-            C = initialPosition.x ** 2 + x1 ** 2 - 2 * initialPosition.x * x1 + initialPosition.y ** 2 - lengthWall ** 2
-            
-            # Determine the delta
-            delta = sqrt(B ** 2 - 4 * A * C)
-            
-            # Get the 2 solutions
-            y1 = (- B + delta) / (2 * A)
-            y2 = (- B - delta) / (2 * A)
+    def createWall(self):
+        # Do the rotation for take the good position of the intersection
+        location = getPositionRotationY(self.object1.location, self.object2.location, math.pi/2)
+        verticesObject1 = intersectionCircleLine(location, self.object1.location, self.wallWidth)
         
-        #Get the position
-        positionWall = mathutils.Vector((x1, y1, 0))
+        # Do the rotation for take the good position of the intersection
+        location = getPositionRotationY(self.object2.location, self.object1.location, math.pi/2)
+        verticesObject2 = intersectionCircleLine(location, self.object2.location, self.wallWidth)
         
-        # Verify if the position is visible by the others points
-        if self.visible(positionWall, initialPosition, self.positionCenter) < 0:
-            # Change position
-            positionWall = mathutils.Vector((x2, y2, 0))
-        
-        # Add the point
-        if len(self.positionsInternalWall) < 3 or not (self.visible(positionWall, self.positionsInternalWall[0], self.positionCenter) > 0 and self.visible(initialPosition, self.positionsInternalWall[0], self.positionCenter) < 0):
+        # Make the wall
+        if verticesObject1[0] != None and verticesObject2 != None:
+            # Do the extrude
+            verticesObject1 += (mathutils.Vector((verticesObject1[0].x, verticesObject1[0].y, verticesObject1[0].z + self.wallHeight)), mathutils.Vector((verticesObject1[1].x, verticesObject1[1].y, verticesObject1[1].z + self.wallHeight)))
+            verticesObject2 += (mathutils.Vector((verticesObject2[0].x, verticesObject2[0].y, verticesObject2[0].z + self.wallHeight)), mathutils.Vector((verticesObject2[1].x, verticesObject2[1].y, verticesObject2[1].z + self.wallHeight)))
+            
             # Do the edges
-            if len(self.positionsInternalWall) > 0:
-                self.listEdges.append((len(self.positionsInternalWall) - 1, len(self.positionsInternalWall)))
+            listEdges = ((0, 1), (0, 2), (1, 3), (2, 3), (4, 5), (4, 6), (5, 7), (6, 7), (0, 4), (1, 5), (2, 6), (3, 7))
             
-            self.positionsInternalWall.append(positionWall)
-            self.createInternalWall(positionWall, random.uniform(self.minimalWallLenght, self.maximalWallLenght))
-        else:
-            self.listEdges.append((len(self.positionsInternalWall) - 1, 0))
-    
-    # Defines the visibility of a point
-    def visible(self, vertex, previousVertex, previousPreviousVertex):
-        segmentActuel = mathutils.Vector((previousPreviousVertex.x - previousVertex.x, previousPreviousVertex.y - previousVertex.y, 0))
-        segmentPrecedent = mathutils.Vector((vertex.x - previousVertex.x, vertex.y - previousVertex.y, 0))
-        
-        # Orientation of the polygon, it's everytime positive
-        signOrientation = 1
-        return signOrientation * (segmentActuel.x * segmentPrecedent.y - segmentActuel.y * segmentPrecedent.x)
-    
-    # Defines the position of the point internal of the wall
-    def createExternalWall(self):
-        index = 0
-        for pos in self.positionsInternalWall:
-            if not (pos.x == self.positionCenter.x and pos.y == self.positionCenter.y):
-                H = (pos.x - self.positionCenter.x) ** 2
-                x1 = sqrt(((self.distance + self.widthWall) ** 2 * H) / (H + (pos.y - self.positionCenter.y) ** 2))
-                y1 = x1 * (pos.y - self.positionCenter.y) / (pos.x - self.positionCenter.x)
-                x2 = - x1
-                y2 = x2 * (pos.y - self.positionCenter.y) / (pos.x - self.positionCenter.x)
-                
-                # Do the edges
-                # Edges between external vertex
-                if len(self.positionsExternalWall) > 0:
-                    self.listEdges.append((len(self.positionsInternalWall) + len(self.positionsExternalWall) - 1, len(self.positionsInternalWall) + len(self.positionsExternalWall)))
-                # Edges between internal and external vertex
-                self.listEdges.append((index, len(self.positionsInternalWall) + len(self.positionsExternalWall)))
-                
-                # Do the faces
-                if len(self.positionsExternalWall) > 0:
-                    self.listFaces.append((index - 1, index, len(self.positionsInternalWall) + len(self.positionsExternalWall), len(self.positionsInternalWall) + len(self.positionsExternalWall) - 1))
-                
-                # Add the new vertex
-                if self.positionCenter.x > pos.x:
-                    if self.positionCenter.y > pos.y:
-                        if self.positionCenter.x > x1 and self.positionCenter.y > y1:
-                            self.positionsExternalWall.append(mathutils.Vector((x1, y1, 0)))
-                        else:
-                            self.positionsExternalWall.append(mathutils.Vector((x2, y2, 0)))
-                    elif self.positionCenter.y < pos.y:
-                        if self.positionCenter.x > x1 and self.positionCenter.y < y1:
-                            self.positionsExternalWall.append(mathutils.Vector((x1, y1, 0)))
-                        else:
-                            self.positionsExternalWall.append(mathutils.Vector((x2, y2, 0)))
-                    else:
-                        if self.positionCenter.x > x1:
-                            self.positionsExternalWall.append(mathutils.Vector((x1, y1, 0)))
-                        else:
-                            self.positionsExternalWall.append(mathutils.Vector((x2, y2, 0)))
-                elif self.positionCenter.x < pos.x:
-                    if self.positionCenter.y > pos.y:
-                        if self.positionCenter.x < x1 and self.positionCenter.y > y1:
-                            self.positionsExternalWall.append(mathutils.Vector((x1, y1, 0)))
-                        else:
-                            self.positionsExternalWall.append(mathutils.Vector((x2, y2, 0)))
-                    elif self.positionCenter.y < pos.y:
-                        if self.positionCenter.x < x1 and self.positionCenter.y < y1:
-                            self.positionsExternalWall.append(mathutils.Vector((x1, y1, 0)))
-                        else:
-                            self.positionsExternalWall.append(mathutils.Vector((x2, y2, 0)))
-                    else:
-                        if self.positionCenter.x < x1:
-                            self.positionsExternalWall.append(mathutils.Vector((x1, y1, 0)))
-                        else:
-                            self.positionsExternalWall.append(mathutils.Vector((x2, y2, 0)))
-                else:
-                    if self.positionCenter.y > pos.y:
-                        if self.positionCenter.y > y1:
-                            self.positionsExternalWall.append(mathutils.Vector((x1, y1, 0)))
-                        else:
-                            self.positionsExternalWall.append(mathutils.Vector((x2, y2, 0)))
-                    else:
-                        if self.positionCenter.y < y1:
-                            self.positionsExternalWall.append(mathutils.Vector((x1, y1, 0)))
-                        else:
-                            self.positionsExternalWall.append(mathutils.Vector((x2, y2, 0)))
-            index += 1
-        
-        # Add the last edges
-        self.listEdges.append((len(self.positionsInternalWall) + len(self.positionsExternalWall) - 1, index - 1))
-        # Add the last faces
-        self.listFaces.append((index - 1, 0, index, len(self.positionsInternalWall) + len(self.positionsExternalWall) - 1))
-    
-    # Create the wall height
-    def createWallHeight(self, object):
-        # Active the object
-        object.select = True
-        bpy.context.scene.objects.active = object
-        
-        # Deselect all vertex
-        bpy.ops.object.mode_set(mode='EDIT')
-        bpy.ops.mesh.select_all(action = 'DESELECT')
-        bpy.ops.object.mode_set(mode='OBJECT')
-        
-        index = 0
-        for face in self.listFaces:
-            # Select the vertex of a face
-            for vertex in object.data.polygons[index].vertices:
-                bpy.context.active_object.data.vertices[vertex].select = True
+            # Do the faces
+            listFaces = [(0, 1, 3, 2), (4, 5, 7, 6), (0, 2, 6, 4), (1, 3, 7, 5), (0, 1, 5, 4), (2, 3, 7, 6)]
             
-            # Do the extrude and after deselect the vertex
-            bpy.ops.object.mode_set(mode='EDIT')
-            bpy.ops.mesh.extrude_region_move(TRANSFORM_OT_translate={"value":mathutils.Vector((0, 0, random.uniform(self.minimalHeightWall, self.maximalHeightWall)))})
-            bpy.ops.mesh.select_all(action = 'DESELECT')
-            bpy.ops.object.mode_set(mode='OBJECT')
-            index += 1
-    
-    # Get the position for the towers
-    def getPositionBaseTowers():
-        # Deselect all vertex
-        bpy.ops.object.mode_set(mode='EDIT')
-        bpy.ops.mesh.select_all(action = 'DESELECT')
-        bpy.ops.object.mode_set(mode='OBJECT')
-        
-        # Select all the vertices of the wall where z == 0
-        vertices = []
-        for vertex in bpy.context.active_object.data.vertices:
-            if vertex.co.z == 0:
-                vertex.select = True
-                vertices.append(vertex)
-        
-        numberVertices = int(len(vertices) / 2)
-        verticesReturns = []
-        for index in range(numberVertices):
-            vertex = mathutils.Vector(((vertices[index].co.x + vertices[index+numberVertices].co.x) / 2, (vertices[index].co.y + vertices[index+numberVertices].co.y) / 2, 0))
-            verticesReturns.append(vertex)
-        
-        # Deselect all vertex
-        bpy.ops.object.mode_set(mode='EDIT')
-        bpy.ops.mesh.select_all(action = 'DESELECT')
-        bpy.ops.object.mode_set(mode='OBJECT')
-        
-        # Active the object
-        bpy.context.active_object.select = False
-        bpy.context.scene.objects.active = None
-        
-        return verticesReturns
+            # Create the meshes
+            object = createMesh("WallGenerator", (0,0,0), verticesObject1 + verticesObject2, listEdges, listFaces)
+            
+
+obejcts = [obj for obj in bpy.context.scene.objects if obj.select]
+WallGenerator(obejcts[0], obejcts[1])
