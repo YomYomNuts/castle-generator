61d56dfb5923d4ecc5b83c0a76a6750b5454d293
noisette.guillaume@gmail.com <noisette.guillaume@gmail.com@3e8a6023-0aef-408e-827d-e1be3dc68d90>
2013-07-12 15:32:34 +0000

    Ajout de la génération du sol

---
+++ trunk/Guillaume/groundGenerator.py
@@ -0,0 +1,26 @@
+import bpy
+from utils import *
+
+class GroundGenerator:
+    def __init__(self):
+        # Find all the vertex that form the ground
+        objects = [x for x in bpy.context.scene.objects]
+        listVertices = []
+        for object in objects:
+            if "TOWER" in object.name.upper():
+                listVertices.append(object.location)
+        
+        # Create the meshes
+        object = createMesh("GroundGenerator", (0,0,0), listVertices, [], [])
+        bpy.context.scene.objects.active = object
+        object.select = True
+        
+        # Do the face
+        bpy.ops.object.mode_set(mode='EDIT')
+        bpy.ops.mesh.select_all(action = 'SELECT')
+        bpy.ops.mesh.edge_face_add()
+        bpy.ops.object.mode_set(mode='OBJECT')
+        if len(object.data.polygons) > 0 and  object.data.polygons[0].normal.z < 0:
+            bpy.ops.object.mode_set(mode='EDIT')
+            bpy.ops.mesh.flip_normals()
+            bpy.ops.object.mode_set(mode='OBJECT')
